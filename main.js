a65fb4736f955f410f2486c4fe7666e466bf31c6
Aditya Raul <78086929+RaulLit@users.noreply.github.com>
2022-01-08 15:52:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 var container = document.getElementById('container');
 var popcontainer = document.getElementById('popcontainer');
 var noOfExercisesInput = document.getElementById('numberOfExercises');
-var timePerExerciseInput = document.getElementById('timePerExercise');
+// var timePerExerciseInput = document.getElementById('timePerExercise');
+var timePerExerciseMin = document.getElementById('timePerExercise-min');
+var timePerExerciseSec = document.getElementById('timePerExercise-sec');
 var restTimeBetweenExerciseInput = document.getElementById('restTimeBetweenExercise');
 var totalWorkoutTime = document.getElementById('totalWorkoutTime');
-
 const startbtn = document.getElementById('startbtn');
 const resetbtn = document.getElementById('resetbtn');
+const stopbtn = document.getElementById('stopbtn');
 
-var noOfExercise;
-var timePerExercise;
-var restTimeBetweenExercise;
+// ***VARIABLES***
+var timePerExerciseInput;
 var totalTime;
+var beep = new Audio("sound/beep.wav");
+var finishSound = document.getElementById('finish-sound');
+
 
 function run() {
-    noOfExercise = noOfExercisesInput.value;
-    timePerExercise = timePerExerciseInput.value;
-    restTimeBetweenExercise = restTimeBetweenExerciseInput.value;
-    if(noOfExercise>1) {
-        totalTime = (noOfExercise * timePerExercise) + ((noOfExercise-1) * restTimeBetweenExercise);
+    timePerExerciseInput = timePerExerciseMin.value*60 + timePerExerciseSec.value;
+    if(noOfExercisesInput.value>1) {
+        totalTime = (noOfExercisesInput.value * timePerExerciseInput) + ((noOfExercisesInput.value-1) * restTimeBetweenExerciseInput.value);
     } else {
-        totalTime = timePerExercise;
+        totalTime = timePerExerciseInput;
     }
-
     var minutes = Math.floor(totalTime/60);
     var seconds = totalTime % 60;
 
-    totalWorkoutTime.innerHTML = `Total Workout Time: ${minutes} : ${seconds}`;
+    totalWorkoutTime.innerHTML = `Total Workout Time: <br> ${minutes} : ${seconds}`;
     container.style.display = "none";
     container.style.visibility = "hidden";
     popcontainer.style.display = "block";
@@ -45,18 +46,25 @@ function start() {
     let time = totalTime;
     var minutes = Math.floor(time/60);
     var seconds = time % 60;
-    totalWorkoutTime.innerHTML = `Total Workout Time: ${minutes} : ${seconds}`;
+    totalWorkoutTime.innerHTML = `Total Workout Time: <br> ${minutes} : ${seconds}`;
     var myInterval = setInterval(updateTime, 1000);
-
+    let sum = 0;
     function updateTime () {
+        if(sum==timePerExerciseInput) {
+            beep.play();
+            sum=0;
+        }
         minutes = Math.floor(time/60);
         seconds = time % 60;
-    
+        ++sum;
         totalWorkoutTime.innerHTML = `${minutes} : ${seconds}`;
         time--;
         if(time<=0) {
             clearInterval(myInterval);
             totalWorkoutTime.innerHTML = "TIME UP!!!";
+            finishSound.play();
+            stopbtn.style.visibility = 'visible';
+            stopbtn.style.display = 'block';
             resetbtn.style.display = 'block';
             resetbtn.style.visibility = 'visible';
         }
@@ -73,6 +81,11 @@ function reset() {
     container.style.visibility = 'visible';
 
     noOfExercisesInput.value = '';
-    timePerExerciseInput.value = '';
+    timePerExerciseMin.value = '';
+    timePerExerciseSec.value = '';
     restTimeBetweenExerciseInput.value = '';
+}
+
+function stopSound() {
+    finishSound.pause();
 }
