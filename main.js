d2d3c04e174540a103e3ec28a2ea5b8653a42f2c
Aditya Raul <78086929+RaulLit@users.noreply.github.com>
2022-01-12 12:37:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var container = document.getElementById('container');
 var popcontainer = document.getElementById('popcontainer');
 var noOfExercisesInput = document.getElementById('numberOfExercises');
-// var timePerExerciseInput = document.getElementById('timePerExercise');
 var timePerExerciseMin = document.getElementById('timePerExercise-min');
 var timePerExerciseSec = document.getElementById('timePerExercise-sec');
 var restTimeBetweenExerciseInput = document.getElementById('restTimeBetweenExercise');
@@ -16,69 +15,22 @@ var totalTime;
 var beep = new Audio("sound/beep.wav");
 var finishSound = document.getElementById('finish-sound');
 
+function appear(container) {
+    container.style.display = "block";
+    container.style.visibility = "visible";
+}
 
-function run() {
-    timePerExerciseInput = timePerExerciseMin.value*60 + timePerExerciseSec.value;
-    if(noOfExercisesInput.value>1) {
-        totalTime = (noOfExercisesInput.value * timePerExerciseInput) + ((noOfExercisesInput.value-1) * restTimeBetweenExerciseInput.value);
-    } else {
-        totalTime = timePerExerciseInput;
-    }
-    var minutes = Math.floor(totalTime/60);
-    var seconds = totalTime % 60;
-
-    totalWorkoutTime.innerHTML = `Total Workout Time: <br> ${minutes} : ${seconds}`;
+function disappear(container) {
     container.style.display = "none";
     container.style.visibility = "hidden";
-    popcontainer.style.display = "block";
-    popcontainer.style.visibility = "visible";
-    resetbtn.style.display = "block";
-    resetbtn.style.visibility = "visible";
-    startbtn.style.display = "block";
-    startbtn.style.visibility = "visible";
 }
 
-function start() {
-    startbtn.style.display = 'none';
-    startbtn.style.visibility = 'hidden';
-    resetbtn.style.display = 'none';
-    resetbtn.style.visibility = 'hidden';
-    let time = totalTime;
-    var minutes = Math.floor(time/60);
-    var seconds = time % 60;
-    totalWorkoutTime.innerHTML = `Total Workout Time: <br> ${minutes} : ${seconds}`;
-    var myInterval = setInterval(updateTime, 1000);
-    let sum = 0;
-    function updateTime () {
-        if(sum==timePerExerciseInput) {
-            beep.play();
-            sum=0;
-        }
-        minutes = Math.floor(time/60);
-        seconds = time % 60;
-        ++sum;
-        totalWorkoutTime.innerHTML = `${minutes} : ${seconds}`;
-        time--;
-        if(time<=0) {
-            clearInterval(myInterval);
-            totalWorkoutTime.innerHTML = "TIME UP!!!";
-            finishSound.play();
-            stopbtn.style.visibility = 'visible';
-            stopbtn.style.display = 'block';
-            resetbtn.style.display = 'block';
-            resetbtn.style.visibility = 'visible';
-        }
-    }
-}
 function reset() {
-    resetbtn.style.display = "none";
-    resetbtn.style.visibility = "hidden";
-    startbtn.style.display = "none";
-    startbtn.style.visibility = "hidden";
-    popcontainer.style.display = 'none';
-    popcontainer.style.visibility = 'hidden';
-    container.style.display = 'block';
-    container.style.visibility = 'visible';
+    disappear(stopbtn);
+    disappear(resetbtn);
+    disappear(startbtn);
+    disappear(popcontainer);
+    appear(container);
 
     noOfExercisesInput.value = '';
     timePerExerciseMin.value = '';
@@ -88,4 +40,94 @@ function reset() {
 
 function stopSound() {
     finishSound.pause();
+}
+
+function run() {
+    if(timePerExerciseSec.value<1) timePerExerciseSec.value = "";
+    timePerExerciseInput = ((timePerExerciseMin.value*60) + timePerExerciseSec.value);
+    if(noOfExercisesInput.value>=1) {
+        totalTime = noOfExercisesInput.value * timePerExerciseInput;
+    } else {
+        totalTime = timePerExerciseInput;
+    }
+    var minutes = Math.floor(totalTime/60);
+    var seconds = totalTime % 60;
+    seconds = (seconds<10)? "0"+seconds : seconds;
+    totalWorkoutTime.innerHTML = `Total Workout Time: <br> ${minutes} : ${seconds}`;
+    disappear(container);
+    appear(popcontainer);
+    appear(resetbtn);
+    appear(startbtn);
+}
+function start() {
+    disappear(startbtn);
+    disappear(resetbtn);
+    let workouts = noOfExercisesInput.value;
+    initiate();
+    function initiate() {
+        workouts--;
+        var timer1 = new Timer(timePerExerciseInput, printTimer, afterFunc);
+        timer1.runTimer();
+    }
+    function afterFunc () {
+        if(workouts <= 0) {
+            totalWorkoutTime.innerHTML = "TIME UP!!!";
+            finishSound.play();
+            appear(stopbtn);
+            appear(resetbtn);
+            return;
+        }
+        beep.play();
+        totalWorkoutTime.innerHTML = "Take a break";
+        rest();
+    }
+    function printTimer(time) {
+        let min = Math.floor(time/60);
+        let sec = time % 60;
+        sec = (sec<10)? "0"+sec : sec;
+        if(min <= 0) {
+            totalWorkoutTime.innerHTML = `Do the Workout!! <br> ${sec}`;
+        } else {
+            totalWorkoutTime.innerHTML = `Do the Workout!! <br> ${min} : ${sec}`;
+        }
+    }
+    function printRest(time) {
+        let min = Math.floor(time/60);
+        let sec = time % 60;
+        sec = (sec<10)? "0"+sec : sec;
+        if(min <= 0) {
+            totalWorkoutTime.innerHTML = `Take a Break! <br> ${sec}`;
+        } else {
+            totalWorkoutTime.innerHTML = `Take a Break! <br> ${min} : ${sec}`;
+        }
+    }
+    function rest() {
+        var restTime = new Timer(restTimeBetweenExerciseInput.value, printRest, 
+            ()=>{
+                beep.play();
+                totalWorkoutTime.innerHTML = "Start!!";
+                initiate();
+            });
+        restTime.runTimer();
+    }
+}
+
+class Timer {
+    constructor(timeInSec, printTime, afterFunction) {
+        this.time = timeInSec;
+        this.printTime = printTime;
+        this.afterFunc = afterFunction;
+    }
+
+    runTimer() {
+        let timer = this.time;
+        var myInterval = setInterval(()=>{
+            this.printTime(timer);
+            timer--;
+            if(timer<0) {
+                clearInterval(myInterval);
+                this.afterFunc();
+            }
+        }, 1000);
+    }
 }
